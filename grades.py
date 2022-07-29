9fe9f71c49741bd17613e5a6b6e46da32b0688aa
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-07-29 13:55:51 -0500

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,188 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+
+body {
+    text-align:center;
+}
+
+table{
+  background-color: white;
+  border-radius: 3px;
+  font-family: 'Open Sans', sans-serif;
+  font-size:18px;
+  background-color:#34495e;
+  table-layout: fixed;
+  margin:30px auto;
+
+}
+
+td, th{
+  padding:18px;
+  border-radius: 3px;
+  background-color: #2c3e50;
+  color:white;
+  box-shadow: 2px 2px 5px #34495e;
+  text-align:center;
+}
+
+th{
+  background-color: #2980b9;
+}
+</style>
+
+<table><tbody>
+<tr>
+<th> Name </th>
+<th> Marks </th>
+<th> Grade </th>
+</tr>
+<tr>
+<td> Jon </td>
+<td> 65 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Dave </td>
+<td> 23 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Thomas </td>
+<td> 98 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Max </td>
+<td> 95 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Tom </td>
+<td> 58 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Becky </td>
+<td> 19 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Penny </td>
+<td> 91 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Shreya </td>
+<td> 30 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Lisa </td>
+<td> 60 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> George </td>
+<td> 85 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Rihana </td>
+<td> 74 </td>
+<td> -- </td>
+</tr>
+</tbody></table>
+Here are Tom's grades:
+<table><tbody>
+<tr>
+<th> Name </th>
+<th> Marks </th>
+</tr>
+<tr>
+<td> Tom </td>
+<td> 58 </td>
+</tr>
+</tbody></table>
+Students who are passing:
+<table><tbody>
+<tr>
+<th> Name </th>
+<th> Marks </th>
+<th> Grade </th>
+</tr>
+<tr>
+<td> Jon </td>
+<td> 65 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Thomas </td>
+<td> 98 </td>
+<td> A+ </td>
+</tr>
+<tr>
+<td> Max </td>
+<td> 95 </td>
+<td> A </td>
+</tr>
+<tr>
+<td> Penny </td>
+<td> 91 </td>
+<td> A- </td>
+</tr>
+<tr>
+<td> Lisa </td>
+<td> 60 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> George </td>
+<td> 85 </td>
+<td> -- </td>
+</tr>
+<tr>
+<td> Rihana </td>
+<td> 74 </td>
+<td> -- </td>
+</tr>
+</tbody></table>
+Students who are failing:
+<table><tbody>
+<tr>
+<th> Name </th>
+<th> Marks </th>
+<th> Grade </th>
+</tr>
+<tr>
+<td> Dave </td>
+<td> 23 </td>
+<td> Fail </td>
+</tr>
+<tr>
+<td> Tom </td>
+<td> 58 </td>
+<td> Fail </td>
+</tr>
+<tr>
+<td> Becky </td>
+<td> 19 </td>
+<td> Fail </td>
+</tr>
+<tr>
+<td> Shreya </td>
+<td> 30 </td>
+<td> Fail </td>
+</tr>
+</tbody></table>
+Student with highest grade:
+<table><tbody>
+<tr>
+<th> Name </th>
+<th> max(Marks) </th>
+</tr>
+<tr>
+<td> Thomas </td>
+<td> 98 </td>
+</tr>
+</tbody></table>
